052289ae845eb9451a3e57a58890c60a9e57488f
Yukio Sekimoto <sekimoto@beacrew.jp>
2018-04-27 12:17:59 +0900

    v2.0.1として追加

---
+++ v2_0_1/sdk/BeacrewLoco/BeacrewLoco.framework/Headers/BCLError.h
@@ -0,0 +1,36 @@
+//
+//  BCLError.h
+//  BeacrewLoco
+//
+//  Created by Yukio Sekimoto on 2016/11/02.
+//  Copyright (c) 2016 Beacrew Inc. All rights reserved.
+//
+
+#ifndef BeacrewLoco_BCLError_h
+#define BeacrewLoco_BCLError_h
+
+#import <Foundation/Foundation.h>
+
+/**
+ エラーの情報を保持するクラスです。
+ */
+@interface BCLError : NSObject
+
+/**
+ エラーコード
+ */
+@property NSInteger code;
+
+/**
+ エラーメッセージ
+ */
+@property NSString *message;
+
+/**
+ エラー詳細（詳細情報が無い場合はnilが入ります）
+ */
+@property NSError *detail;
+
+@end
+
+#endif
